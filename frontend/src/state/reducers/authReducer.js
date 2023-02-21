dfc37031c160518d7d62973faeabb190aa1c7bc2
viro <twikista@gmail.com>
2023-02-21 06:11:24 +0100

    added authReducer

--- frontend/src/state/reducers/authReducer.js
+++ frontend/src/state/reducers/authReducer.js
@@ -1,3 +1,15 @@
+import { LOGIN_USER, LOGOUT_USER } from "../../constants/authActions";
+
 export default function authReducer(state, action) {
+  //handle user login
+  if (action.type === LOGIN_USER) {
+    return { user: action.payload };
+  }
+
+  //hnadle user logout
+  if (action.type === LOGIN_USER) {
+    return { user: null };
+  }
+
   return state;
 }
