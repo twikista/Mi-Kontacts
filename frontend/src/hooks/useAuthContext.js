5b1e1c3eb5aa0096bfa5013544a7bad967442b4d
viro <twikista@gmail.com>
2023-02-22 10:02:23 +0100

    added useAuthContext hook

---
+++ frontend/src/hooks/useAuthContext.js
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import { AuthContext } from "../state/context/AuthContext";
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw Error("context can't be use outside AuthContxtprovider");
+  }
+  return context;
+};
