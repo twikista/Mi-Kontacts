1929d821a4744d781768fad48b28c261ff8dff13
viro <twikista@gmail.com>
2023-03-25 15:59:20 +0100

    added functionality to  add new contact from frotend

--- frontend/src/pages/CreateContact/CreateContact.jsx
+++ frontend/src/pages/CreateContact/CreateContact.jsx
@@ -1,46 +1,74 @@
-import { useMutation, QueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 const CreateContact = () => {
+  const queryClient = useQueryClient();
+  const [contactAvatar, setContactAvatar] = useState("");
+  const { user } = useAuthContext();
+  // console.log(user);
   const {
     register,
     handleSubmit,
     reset,
-    watch,
     setError,
     formState: { errors },
   } = useForm();
-  const handle = () => "createuser";
-  const filesize = watch("image");
-  console.log(filesize);
-  const valFile = (file) => {
-    if (file?.length) {
-      const fileLength = file?.length;
-      const fileSize = file?.[0].size < 5000;
-      return fileLength && !fileSize ? "Max 10MB" : null;
+
+  const onChangehandler = (e) => {
+    const file = e.target.files[0];
+    if (!["image/jpg", "image/jpeg", "image/png"].includes(file?.type)) {
+      setError("image", { type: "type", message: "Only PNG, JPEG,  GIF" });
+      return;
     }
-    return null;
+    if (file?.size < 50000) {
+      setError("image", { type: "size", message: null });
+      imageToBase64(file);
+      return;
+    }
+    setError("image", { type: "size", message: "Max 10MB" });
+    setContactAvatar("");
   };
-  // console.log(filesize?.[0].size < 5000);
-  // const
+
+  const imageToBase64 = (file) => {
+    const reader = new FileReader();
+    if (file) {
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        setContactAvatar(reader.result);
+      };
+    } else {
+      setContactAvatar("");
+    }
+  };
+
   const onSubmit = (data) => {
-    const { firstName, lastName, phone, email, city, category } = data;
-    // const file = data.image[0];
-    // console.log(file.size);
-    // if (!filesize) {
-    //   setError("image", { type: "size", message: "small file size" });
-    //   return;
-    // }
-    const contact = {
-      firstName,
-      lastName,
-      contactInfo: { phone, email, city },
-      category,
-    };
-    console.log(data);
+    const contact = { ...data, image: contactAvatar };
+    createContact(contact);
+    console.log(contact);
     reset();
   };
-  const { mutate: createContact } = useMutation({ mutationFn: handle });
+
+  const setContact = async (contactDetails) => {
+    const response = await fetch("http://localhost:4000/api/contacts/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.token}`,
+      },
+      body: JSON.stringify(contactDetails),
+    });
+    const contact = await response.json();
+    return contact;
+  };
+
+  const { mutate: createContact } = useMutation({
+    mutationFn: setContact,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["contacts"] });
+    },
+  });
 
   return (
     <>
@@ -61,13 +89,25 @@ const CreateContact = () => {
           />
         </div>
         <div>
-          <input {...register("phone")} type="tel" placeholder="Phone number" />
+          <input
+            {...register("contactInfo.phone")}
+            type="tel"
+            placeholder="Phone number"
+          />
         </div>
         <div>
-          <input {...register("email")} type="email" placeholder="Email" />
+          <input
+            {...register("contactInfo.email")}
+            type="email"
+            placeholder="Email"
+          />
         </div>
         <div>
-          <input {...register("city")} type="text" placeholder="City" />
+          <input
+            {...register("contactInfo.city")}
+            type="text"
+            placeholder="City"
+          />
         </div>
         <div>
           <input {...register("categoty")} type="text" placeholder="Category" />
@@ -76,19 +116,31 @@ const CreateContact = () => {
           <input
             {...register("image", {
               validate: {
-                lessThan10MB: (files) => files?.[0].size < 5000 || "max 10MB",
+                lessThan10MB: (files) => files[0]?.size < 5000 || "max 10MB",
+                acceptedFormats: (files) =>
+                  ["image/jpeg", "image/png", "image/gif"].includes(
+                    files[0]?.type
+                  ) || "accpeted image formats: png, jpg/jpeg, gif",
               },
             })}
             type="file"
-            accept="image/png, image/jpg,image/jpeg"
-            placeholder="Category"
+            onChange={onChangehandler}
+            // accept="image/png, image/jpg,image/jpeg, image/gif"
           />
-          {<span>{valFile(filesize)}</span>}
-          {/* {!filesize && <span>Max 10MB</span>} */}
+          {/* {errors?.image ? (
+            <span>{errors.image.message}</span>
+          ) : (
+            <span>{valFile(filesize)}</span>
+          )} */}
+          {errors?.image && <span>{errors.image.message}</span>}
+          {errors?.image?.type === "type" && (
+            <span>{errors.image.type.message}</span>
+          )}
         </div>
 
         <button type="submit">Add User</button>
       </form>
+      <img src={contactAvatar} />
     </>
   );
 };
